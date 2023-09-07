2ca50285d12a7ac699db5aa57e35c9e06378dc08
Diogu Ikechukwu Isaac <dioguikechukwuisaac@gmail.com>
2023-09-07 13:33:03 +0100

    Done with basic header outline

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+const Home = () => {
+  const title = "Vignette";
+  return (
+    <div className="home">
+      <div className="titleDiv">
+        <h1 className="title"> {title} </h1>
+        <p className="motto">Make your streams more virtual</p>
+        <p className="headWriteUp">Free and Open Source</p>
+        <button className="headBtn">Download</button>
+        <div className="lowerHeader">
+          <p>Learn More</p>
+
+          <span>
+            <i class="fa-solid fa-angle-down"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
