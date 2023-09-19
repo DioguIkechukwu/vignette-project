d9fc9d03b7f9d5b8184f5bee6bb3a85e3dfbbb98
Diogu Ikechukwu Isaac <dioguikechukwuisaac@gmail.com>
2023-09-19 13:10:17 +0100

    update

--- src/About.js
+++ src/About.js
@@ -8,13 +8,7 @@ const About = () => {
           VTuber career a more accessible path.
         </p>
         <p>Made for you, by you.</p>
-        <p>
-          And that was our mission: to build a product to make these independent
-          content creators become their own VTubers without using any
-          specialized hardware and relatively low price. We won't gather data
-          from you or sell DLCs, or give a half-complete product to you.
-          Vignette and it's entire features will be forever free, no gotchas.*
-        </p>
+        <p></p>
       </div>
       <div className="aboutImages"></div>
     </div>
